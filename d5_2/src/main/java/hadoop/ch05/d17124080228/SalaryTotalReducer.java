8e719a2046b9a6ac1752a217d02d6123581c63b8
panisfl <40904008+panisfl@users.noreply.github.com>
2019-10-13 18:38:39 +0800

    Add files via upload

---
+++ d5_2/src/main/java/hadoop/ch05/d17124080228/SalaryTotalReducer.java
@@ -0,0 +1,20 @@
+package hadoop.ch05.d17124080228;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class SalaryTotalReducer extends Reducer<NullWritable,Employe,NullWritable,Text> {
+
+    @Override
+    protected void reduce(NullWritable k3, Iterable<Employe> v3,
+                          Context context) throws IOException, InterruptedException {
+        String line=null;
+        for (Employe v : v3) {
+            line = v.toString();
+            context.write(k3, new Text(line));
+        }
+    }
+}
